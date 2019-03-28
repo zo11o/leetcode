108daccd66eef98daf4e49469f2d37e245beab31
zo11o <zhuowenjian@inboyu.com>
2019-03-28 16:33:27 +0800

    字符串反转

---
+++ dev/leetcode605/index.js
@@ -0,0 +1,18 @@
+// 数字反转
+var reverse = function (x) {
+  let result = 0
+
+  while (x !== 0) {
+    result = result * 10 + x % 10
+    x = parseInt(x / 10)
+  }
+
+  if (result < -Math.pow(2, 31) || result > Math.pow(2, 31) - 1) {
+    return 0
+  }
+
+  return result
+}
+
+var a = -225
+console.log(reverse(a))
