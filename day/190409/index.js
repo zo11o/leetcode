3d3bf51f07dce033dd18c877bf96c2790cc2acdf
zo11o <zhuowenjian@inboyu.com>
2019-04-09 14:19:10 +0800

    【day】

--- day/190409/index.js
+++ day/190409/index.js
@@ -1,10 +1,12 @@
+
 Number.prototype.digits = function() {
   return (
     this.valueOf()
       .toString()
       .split(".")[1] || ""
   ).length;
-};
+}
+
 Number.prototype.add = function(i = 0) {
   if (typeof i !== "number") {
     throw new Error("请输入正确的数字");
@@ -14,7 +16,8 @@ Number.prototype.add = function(i = 0) {
   const iDigits = i.digits();
   const baseNum = Math.pow(10, Math.max(thisDigits, iDigits));
   return (v * baseNum + i * baseNum) / baseNum;
-};
+}
+
 Number.prototype.minus = function(i = 0) {
   if (typeof i !== "number") {
     throw new Error("请输入正确的数字");
@@ -24,7 +27,7 @@ Number.prototype.minus = function(i = 0) {
   const iDigits = i.digits();
   const baseNum = Math.pow(10, Math.max(thisDigits, iDigits));
   return (v * baseNum - i * baseNum) / baseNum;
-};
+}
 
 console.log((5).add(3).minus(2))
 console.log((5.3).add(3.2).minus(2.6))
