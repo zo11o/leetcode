f997d588d688f95d51f096d8c0ebaf359173961c
zo11o <372526745@qq.com>
2019-08-14 15:15:12 +0800

    冒泡排序

---
+++ code/sort/bubbleSort.js
@@ -0,0 +1,21 @@
+/**
+ * 冒泡排序
+ */
+function bubbleSort(arr) {
+  // arr.sort((a, b) => a - b);
+  let len = arr.length;
+  for (var i = 0; i < len; i++) {
+    for (var j = 0; j < len - 1 - i; j++) {
+      let temp
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j]
+        arr[j] = arr[j+1]
+        arr[j + 1] = temp
+      }
+    }
+  }
+  return arr
+}
+
+var arr = [33, 55, 1, 4, 77, 24, 0, 55, 2, 4, 6]
+console.log(bubbleSort(arr))
---
+++ leetcode/8.字符串转换整数-atoi.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode.cn id=8 lang=javascript
+ *
+ * [8] 字符串转换整数 (atoi)
+ */
+/**
+ * @param {string} str
+ * @return {number}
+ */
+var myAtoi = function(str) {
+    
+};
+
