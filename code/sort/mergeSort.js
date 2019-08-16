331ed6899805f13a96fcce56af7679a7d0756c7b
zo11o <372526745@qq.com>
2019-08-16 11:06:32 +0800

    实现归并排序

--- code/sort/mergeSort.js
+++ code/sort/mergeSort.js
@@ -7,22 +7,22 @@
  * @description https://www.cnblogs.com/Unknw/p/6346681.html
  */
 function mergeSort(arr) {
-  //采用自上而下的递归方法
-  var len = arr.length
+  let len = arr.length
   if (len < 2) {
     return arr
   }
-  var middle = Math.floor(len / 2),
-    left = arr.slice(0, middle),
-    right = arr.slice(middle)
-  console.log("left", left)
-  console.log("right", right)
+  let middle = Math.floor(len / 2)
+
+  let left = arr.slice(0, middle)
+  let right = arr.slice(middle)
+
   return merge(mergeSort(left), mergeSort(right))
 }
 
 function merge(left, right) {
-  var result = []
+  let result = []
 
+  // 比较大小
   while (left.length && right.length) {
     if (left[0] <= right[0]) {
       result.push(left.shift())
@@ -30,16 +30,16 @@ function merge(left, right) {
       result.push(right.shift())
     }
   }
+  while (left.length) {
+    result.push(left.shift())
+  }
 
-  while (left.length) result.push(left.shift())
+  while (right.length) {
+    result.push(right.shift())
+  }
 
-  while (right.length) result.push(right.shift())
-
-  console.log('result=====',result)
   return result
 }
-
-// TODO: 归并排序自己实现。以上为网页实现
 
 var arr = [33, 55, 1, 4, 77, 24, 0, 55, 2, 4, 6]
 console.log(mergeSort(arr))
