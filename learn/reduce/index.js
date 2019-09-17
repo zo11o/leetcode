1c5b49fe87944a8c066f0601b67d14671f665a3a
zo11o <372526745@qq.com>
2019-09-17 15:59:24 +0800

    累加器

--- learn/reduce/index.js
+++ learn/reduce/index.js
@@ -4,10 +4,13 @@
     arr.push(i)
   }
   console.log(arr)
+  const body = document.body;
   var b = arr.reduce(function(accumulator, currentValue, index) {
     console.log("accumulator", accumulator)
     console.log("currentValue", currentValue)
     console.log("index", index)
+    var dom = document.createTextNode(`${accumulator + currentValue}\n\r`);
+    body.appendChild(dom)
     return accumulator + currentValue
   }, 1)
 
